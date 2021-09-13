172405ed473dd565153116c34a96c8f8dfed102c
R8V <islaterm@gmail.com>
2021-09-12 22:56:18 -0300

    feat(engine)  | NUnit test runner | 0.0.0.1 | WORKING

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+﻿# Changelog
+
+## Version 0.x
+
+| Category      | Detail            | Version | Status  |
+| ------------- | ----------------- | ------- | ------- |
+| feat(engine)  | NUnit test runner | 0.0.0.1 | WORKING |
+| feat(funspec) | dropWhile         | 0.0.3.2 | WORKING |
+| feat(funspec) | SourceRef         | 0.0.3.1 | BROKEN  |
+| feat(funspec) | More definitions  | 0.0.2   | WORKING |
+| feat(funspec) | Basic definitions | 0.0.1   | WORKING |
---
+++ src/Runner/NUnit/Platform/NUnitRunner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework.Internal;
+
+namespace GoDoTest.Runner.NUnit.Platform {
+  /// <summary>
+  ///   Base for creating custom test runners.
+  /// </summary>
+  public interface INUnitRunner {
+    /// <summary>
+    ///   Called before a group of properties on a type are checked.
+    /// </summary>
+    void OnStartFixture(Type type);
+
+    /// <summary>
+    ///   Called whenever arguments are generated and after the test is run.
+    /// </summary>
+    void OnArguments(int nTest, List<object> args, Func<int, List<object>, string> every);
+
+    /// <summary>
+    ///   Called on a successful shrink.
+    /// </summary>
+    void OnShrink(List<object> args, Func<List<object>, string> everyShrink);
+
+    /// <summary>
+    ///   Called whenever all tests are done, either True, False or Exhausted.
+    /// </summary>
+    void OnFinished(string _, TestResult testResult);
+  }
+}
